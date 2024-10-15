a4310c372117cd863a7db41971c485b0f2e6c334
MDMcodes <matheusdonadelmarques08@gmail.com>
2024-10-15 14:00:55 -0300

    update, tarefa finalizada

--- ex01.py
+++ ex01.py
@@ -23,28 +23,65 @@ def criar_na_tabela():
     cursor.execute(comando_sql)
     conexao_banco.commit()
 
-#DELETE
+# #DELETE
 def deletar_na_tabela():
-    id_input = int(input('Digite o ID do carro a ser deletado: '))
-    comando_sql = 'SELECT * FROM produtos'
+    id_input = int(input('Digite o ID do carro que deseja deletar: '))
+    comando_sql = f'DELETE FROM produtos WHERE idprodutos = {id_input}'
     cursor.execute(comando_sql)
-    dados_tabela = cursor.fetchall()
-    for dado in dados_tabela:
-        if dado[0] == id_input:
-            comando_sql = f'DELETE FROM produtos WHERE idprodutos = {id_input}'
-            cursor.execute(comando_sql)
-            conexao_banco.commit()
-            print(f'O carro foi deletado com sucesso')
-            break
-        else:
-            print('Não encontrado')
-            break
-
+    conexao_banco.commit()
 #SEARCH
 def buscar_na_tabela():
     print('Pesquisar por: ')
     print('ID = 1')
     print('MARCA = 2')
     print('MODELO = 3')
-    print('ANO = 4')
-    opc = int(input()
+    opc = int(input(f"Selecione sua opção: "))
+    if opc == 1:
+        id_input = int(input('Digite o ID do carro a ser pesquisado: '))
+        comando_sql = 'SELECT * FROM produtos WHERE idprodutos = %s'
+        cursor.execute(comando_sql, (id_input,))
+        dados_tabela = cursor.fetchall()
+        for dado in dados_tabela:
+            print(dado)
+    elif opc == 2:
+        marca_input = input("Digite a marca do carro a ser pesquisado: ")
+        comando_sql = 'SELECT * FROM produtos WHERE marca = %s'
+        cursor.execute(comando_sql, (marca_input,))
+        dados_tabela = cursor.fetchall()
+        for dado in dados_tabela:
+            print(dado)
+    elif opc == 3:
+        modelo_input = input('Digite o modelo do carro a ser pesquisado: ')
+        comando_sql = 'SELECT * FROM produtos WHERE modelo = %s'
+        cursor.execute(comando_sql, (modelo_input,))
+        dados_tabela = cursor.fetchall()
+        for dado in dados_tabela:
+            print(dado)
+    else:
+        print('Opção inválida')
+        buscar_na_tabela()
+        conexao_banco.commit()
+
+
+while True:
+    print('SELECIONE A OPÇÃO DESEJADA: ')
+    print('1 - CADASTRAR CARRO')
+    print('2 - PESQUISAR CARROS')
+    print('3 - DELETAR CARRO')
+    print('4 - SAIR')
+    opc = int(input(f"Selecione sua opção: "))
+    if opc == 1:
+        criar_na_tabela()
+    elif opc == 2:
+        buscar_na_tabela()
+    elif opc == 3:
+        deletar_na_tabela()
+    elif opc == 4:
+        print('Encerrando programa....')
+        break
+    else:
+        print('Opção inválida')  
+
+
+
+
