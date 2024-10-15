0d201502049ca91d8f2f16d369645f49d2abc259
MDMcodes <matheusdonadelmarques08@gmail.com>
2024-10-15 17:28:09 -0300

    update4

--- ex04.py
+++ ex04.py
@@ -31,3 +31,13 @@ def cadastrar_reserva():
 # (data_checkin BETWEEN '{data_checkin_input}' AND '{data_checkout_input}' OR data_checkout BETWEEN '{data_checkin_input}' AND '{data_checkout_input}'): Essa parte do comando é um pouco mais complexa. Ela verifica se a data de check-in ou a data de check-out está dentro do intervalo de datas digitado pelo usuário.
 
 # BETWEEN: é um operador que verifica se um valor está dentro de um intervalo.
+
+def alterar_data():
+    id_input = int(input('Digite o ID da reserva: '))
+    comando_sql = f'SELECT * FROM reservas WHERE ID = {id_input}'
+    cursor.execute(comando_sql)
+    dados_tabela = cursor.fetchall
+    if dados_tabela:
+        data_checkin_input = input('Digite a nova data de check-in: ')
+        data_checkout_input = input('Digite a nova data de check-out')
+        comando_sql = f'UPDATE reservas SET data_checkin = "{data_checkin_input}" AND SET data_checkout = "{data_checkout_input}"'
